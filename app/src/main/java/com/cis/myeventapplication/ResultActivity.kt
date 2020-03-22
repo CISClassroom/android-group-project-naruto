4a1c488905a203e699ecbcb25f05d9450b9db689
sasikorn-dream <sasikorn.k@github.com>
2020-03-22 23:47:39 +0700

    update

--- app/src/main/java/com/cis/myeventapplication/ResultActivity.kt
+++ app/src/main/java/com/cis/myeventapplication/ResultActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
--- app/src/main/java/com/cis/myeventapplication/StudentActivity.kt
+++ app/src/main/java/com/cis/myeventapplication/StudentActivity.kt
@@ -2,11 +2,31 @@ package com.cis.myeventapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class StudentActivity : AppCompatActivity() {
+
+    lateinit var listview : ListView
+    private val TAG:String = "Student Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
+
+
+        listview = findViewById(R.id.listview)
+        val event = arrayOf("comseeit","comcamp")
+        listview.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,event)
+
+        //listview.setOnItemClickListener{parent: AdapterView<*>?, view: View?, position: Int, id: Long ->  }
+
     }
 }
